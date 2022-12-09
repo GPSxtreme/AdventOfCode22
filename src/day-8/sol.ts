a54d5575807f110c1a51f203b1336bb01275b986
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2022-12-09 13:52:23 +0530

    completed day-8

--- src/day-8/sol.ts
+++ src/day-8/sol.ts
@@ -2,8 +2,9 @@ const input = require("fs").readFileSync("./input.txt", "utf-8") as string;
 let size = input.split("\n")[0].trimEnd().length;
 let count = 4 * size - 4;
 let temp = 0;
+let flag = 0;
+let scenicScore = 0;
 let trees: Array<number>[] = [];
-let visit: Array<number>[] = [];
 input.split("\n").forEach((line) => {
   temp++;
   for (let i = 0; i < size; i++) {
@@ -11,51 +12,86 @@ input.split("\n").forEach((line) => {
     trees[temp - 1][i] = +line[i];
   }
 });
-for (let i = 0; i < size; i++) {
-  for (let j = 0; j < size; j++) {
-    if (visit[i] === undefined) visit[i] = [];
-    if (i === 0 || i === size - 1 || j === 0 || j === size - 1) visit[i][j] = 1;
-    else visit[i][j] = 0;
+//part-1
+for (let column = 1; column < size - 1; column++) {
+  for (let row = 1; row < size - 1; row++) {
+    //front
+    for (let i = row + 1; i <= size - 1; i++) {
+      if (trees[row][column] <= trees[i][column]) {
+        flag++;
+        break;
+      }
+    }
+    //back
+    for (let i = row - 1; i >= 0; i--) {
+      if (trees[row][column] <= trees[i][column]) {
+        flag++;
+        break;
+      }
+    }
+    //right
+    for (let i = column + 1; i <= size - 1; i++) {
+      if (trees[row][column] <= trees[row][i]) {
+        flag++;
+        break;
+      }
+    }
+    //left
+    for (let i = column - 1; i >= 0; i--) {
+      if (trees[row][column] <= trees[row][i]) {
+        flag++;
+        break;
+      }
+    }
+    if (flag < 4) {
+      count++;
+    }
+    flag = 0;
   }
 }
-for (let i = 1; i <= size - 1; i++) {
-  //view from front
-  temp = trees[0][i];
-  for (let j = 1; j < size - 1; j++) {
-    if (trees[i][j] > temp && !visit[i][j]) {
-      count++;
-      visit[i][j] = 1;
+console.log(`Trees visible from outside of the grid: ${count}`);
+//part-2
+let frontVT = 0;
+let backVT = 0;
+let rightVT = 0;
+let leftVT = 0;
+for (let column = 0; column < size - 1; column++) {
+  for (let row = 0; row < size - 1; row++) {
+    //front scenic number
+    for (let i = row - 1; i >= 0; i--) {
+      if (trees[i][column] >= trees[row][column]) {
+        frontVT++;
+        break;
+      } else frontVT++;
     }
-    temp = trees[i][j];
-  }
-  //view from back
-  temp = trees[size - 1][i];
-  for (let j = size - 1; j >= 0; j--) {
-    if (trees[j][i] > temp && !visit[j][i]) {
-      count++;
-      visit[j][i] = 1;
+    //back scenic number
+    for (let i = row + 1; i <= size - 1; i++) {
+      if (trees[i][column] >= trees[row][column]) {
+        backVT++;
+        break;
+      } else backVT++;
     }
-    temp = trees[j][i];
-  }
-  //view from left
-  temp = trees[0][i];
-  for (let j = 1; j < size; j++) {
-    if (trees[i][j] > temp && !visit[i][j]) {
-      count++;
-      visit[i][j] = 1;
+    //right scenic number
+    for (let i = column + 1; i <= size - 1; i++) {
+      if (trees[row][i] >= trees[row][column]) {
+        rightVT++;
+        break;
+      } else rightVT++;
     }
-    temp = trees[i][j];
-  }
-  //view from right
-  temp = trees[size - 1][i];
-  for (let j = size - 1; j >= 1; j--) {
-    if (trees[i][j] > temp && !visit[i][j]) {
-      count++;
-      visit[i][j] = 1;
+    //left scenic score
+    for (let i = column - 1; i >= 0; i--) {
+      if (trees[row][i] >= trees[row][column]) {
+        leftVT++;
+        break;
+      } else leftVT++;
     }
-    temp = trees[i][j];
+    //calculate scenic score and reset variables
+    const score = frontVT * backVT * rightVT * leftVT;
+    if (score > scenicScore) scenicScore = score;
+    frontVT = 0;
+    backVT = 0;
+    leftVT = 0;
+    rightVT = 0;
   }
 }
-// console.log(visit);
-console.log(size * size);
-console.log(count);
+console.log(`The total scenic score is : ${scenicScore}`);
