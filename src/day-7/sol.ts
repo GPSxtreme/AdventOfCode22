d1a1d6906b770ab14dd91fc76a13dd2873b47c82
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2022-12-10 23:58:09 +0530

    completed day-7 part-1

--- src/day-7/sol.ts
+++ src/day-7/sol.ts
@@ -1,100 +1,58 @@
 const input = require("fs").readFileSync("./input.txt", "utf-8") as string;
 //variables
 const dirOrder: string[] = [];
-let dirTree: object = {};
-let dirTreeWithSize: object = {};
-let dirSizeTree: object = {};
 let pwd: string;
-let answer = 0;
+let allDirs = new Map<string, Array<string | number>>();
+let ansPA = 0;
 //function to check if a char is an integer or not
-function isNumeric(n: string) {
-  return !isNaN(parseFloat(n));
-}
 //collecting data from input
 input.split("\n").forEach((line) => {
   const cmd = line.trim().split(" ");
-  if (cmd[1] === "ls" && cmd[0] === "$") {
-    return;
-  } else if (cmd[0] === "$") {
-    if (cmd[2] === "..") {
+  if (cmd[0] === "$") {
+    if (cmd[1] === "ls") return;
+    else if (cmd[2] === "..") {
       dirOrder.pop();
+      return;
     } else if (cmd[1] === "cd" && cmd[2] !== "..") {
+      pwd = `${
+        dirOrder.length != 0 ? `${dirOrder[dirOrder.length - 1]}-` : ""
+      }${cmd[2]}`;
       dirOrder.push(cmd[2]);
-      pwd = cmd[2];
+      return;
     }
   } else {
-    if (dirTree[pwd] === undefined) dirTree[pwd] = [];
-    if (dirTreeWithSize[pwd] === undefined) dirTreeWithSize[pwd] = [];
-    if (dirSizeTree[pwd] === undefined) dirSizeTree[pwd] = 0;
+    const arr = allDirs.get(pwd) === undefined ? [] : allDirs.get(pwd);
     if (cmd[0] === "dir") {
-      dirTree[pwd].push(cmd[1]);
-      dirTreeWithSize[pwd].push(cmd[1]);
+      allDirs.set(pwd, [
+        ...arr,
+        `${pwd[2] === undefined ? pwd[0] : pwd.split("-")[1]}-${cmd[1]}`,
+      ]);
     } else {
-      dirTree[pwd].push(cmd[0]);
-      dirTreeWithSize[pwd].push(cmd[0]);
+      allDirs.set(pwd, [...arr, +cmd[0]]);
     }
   }
 });
-//recursive function to find size of dir
-function sizeOfDir(dir: string, subDir: string): void {
-  dirTreeWithSize[subDir].map((e: string) => {
-    if (!isNumeric(e)) {
-      sizeOfDir(subDir, e);
+const getDirSize = (path: string): number => {
+  const dir = allDirs.get(path);
+  if (!dir) return;
+  let size = 0;
+  for (const item of dir) {
+    if (typeof item === "number") {
+      size += item;
+      continue;
     }
-  });
-  if (dirTreeWithSize[dir].includes(subDir)) {
-    dirTreeWithSize[subDir].map((e: string) => {
-      dirSizeTree[subDir] += +e;
-    });
-    dirTreeWithSize[dir] = dirTreeWithSize[dir].filter(
-      (item: string) => item !== subDir
+    const dirSize = getDirSize(item);
+    size += dirSize;
+    allDirs.set(
+      path,
+      allDirs.get(path)?.map((i) => (i === item ? dirSize : i))
     );
-    dirTreeWithSize[dir].push(dirSizeTree[subDir]);
-    return;
   }
-}
-//traverse
-const dirs = Object.keys(dirTree);
-for (let i = dirs.length - 1; i >= 0; i--) {
-  let hasNoDirs = true;
-  dirTreeWithSize[dirs[i]].map((subDir: string) => {
-    if (!isNumeric(subDir)) {
-      hasNoDirs = false;
-      sizeOfDir(dirs[i], subDir);
-    }
-  });
-  // if (hasNoDirs) {
-  //   dirTreeWithSize[dirs[i]].map((fileSize: string) => {
-  //     dirSizeTree[dirs[i]] += +fileSize;
-  //   });
-  // }
-}
+  return size;
+};
 
-//find total size of each dir and if it is less than 100k add it to result(bottom up)
-for (let dir = dirs.length - 1; dir >= 0; dir--) {
-  dirSizeTree[dirs[dir]] = 0;
-  dirTreeWithSize[dirs[dir]].map((fileSize: string) => {
-    dirSizeTree[dirs[dir]] += +fileSize;
-  });
-  if (dirSizeTree[dirs[dir]] < 100000) {
-    answer += dirSizeTree[dirs[dir]];
-  }
+for (const [path] of allDirs) {
+  const size = getDirSize(path);
+  if (size < 100000) ansPA += size;
 }
-//checking if root dir size matches with all children dirs size
-const rootDirSize = dirSizeTree["/"];
-let childDirSize = 0;
-Object.keys(dirSizeTree).forEach((key) => {
-  if (key === "/") return;
-  else {
-    childDirSize += dirSizeTree[key];
-  }
-});
-console.log(
-  `rootDir size :${rootDirSize} , childDir size : ${childDirSize} , conditionMet? : ${
-    rootDirSize >= childDirSize
-  }`
-);
-console.log(dirTree);
-console.log(dirTreeWithSize);
-console.log(dirSizeTree);
-console.log(answer);
+console.log(`Sum of the total sizes of directories less than 100k: ${ansPA}`);
